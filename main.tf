59f601c9a1c3889a3a52bf94fa103a3e280e9ec4
Jim Jonah <jimjonah@gmail.com>
2023-11-06 15:28:23 -0500

    update ALB to version 6

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_instance" "blog" {
 
 module "bloc_alb" {
   source = "terraform-aws-modules/alb/aws"
-  version = "~> 5.0"
+  version = "~> 6.0"
 
   name            = "my-alb"
   vpc_id          = module.blog_vpc.vpc_id
